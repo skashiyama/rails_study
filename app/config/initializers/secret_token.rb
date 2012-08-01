041ca8cd9e41d6184e8aaaf92ff6990428ae84d9
skas <Shigeki.Kashiyama@jrits.ricoh.co.jp>
2012-08-01 15:08:15 +0900

    create rails directories

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '658ff826b0cd51749d33037ff096b338aa345297ca4ab59a136d5cfafa16b81e4cfb9003d94d95045d3f9d3bfeb82988b9b72ef71cae3f7d4628375b5281eb06'
